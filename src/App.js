8eb17003e7f83a8bdb87402b1471589ac13a7976
rocky <finnslighthouse@gmail.com>
2020-09-02 17:24:19 -0700

    ready to modify todolist

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import toDoList from './components/TodoList';
 
 class App extends React.Component {
- render() {
-    return (
-      <div>
-      Hello world.
-      </div>
-    );
-  }
-}
+
+  constructor(){
+    super();
+    this.state = {
+     todoList: [{
+      task:"",
+      id:"",
+      completed: false
+     }]
+    };
+
+
+  };
+
+
+  render(){
+    return(
+     <div>
+      <h1>TASKMASTER</h1>
+     </div>
+    )}
+};
 
 export default App;
